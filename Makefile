4954b22f3138306a2c7f75c9bb05547912626607
magis-techo <magis.techno@gmail.com>
2025-05-29 20:27:10 +0800

    update

--- Makefile
+++ Makefile
@@ -12,3 +12,9 @@ init-db:
 	  exec -T workspace \
 	  env PGPASSWORD=postgres \
 	  psql -h local_pg -U postgres -f sql/00_init_local_pg.sql
+
+# Initialise FDW without hard‑coding credentials (.env provides FDW_USER / FDW_PWD)
+fdw-init:
+	docker compose -f docker/docker-compose.yml exec -T workspace \
+	  env PGOPTIONS="-v fdw_user=$$FDW_USER -v fdw_pwd=$$FDW_PWD" \
+	  psql -h local_pg -U postgres -f sql/01_fdw_remote.sql
--- sql/01_fdw_remote.sql
+++ sql/01_fdw_remote.sql
@@ -1,9 +1,20 @@
--- Example FDW mapping template (fill host/user/pwd)
+-- Foreign‑table mapping for trajectory & intersection layers
 CREATE EXTENSION IF NOT EXISTS postgres_fdw;
 
+-- trajectory DB
 CREATE SERVER traj_srv FOREIGN DATA WRAPPER postgres_fdw
   OPTIONS (host 'REMOTE_TRAJ_HOST', dbname 'trajdb', port '5432');
 CREATE USER MAPPING FOR postgres SERVER traj_srv
-  OPTIONS (user 'readonly', password '***');
-IMPORT FOREIGN SCHEMA public LIMIT TO (traj_tracks)
-  FROM SERVER traj_srv INTO public;
+  OPTIONS (user :fdw_user, password :fdw_pwd);
+IMPORT FOREIGN SCHEMA public
+       LIMIT TO (ddi_data_points)
+       FROM SERVER traj_srv INTO public;
+
+-- intersection DB
+CREATE SERVER map_srv FOREIGN DATA WRAPPER postgres_fdw
+  OPTIONS (host 'REMOTE_MAP_HOST', dbname 'mapdb', port '5432');
+CREATE USER MAPPING FOR postgres SERVER map_srv
+  OPTIONS (user :fdw_user, password :fdw_pwd);
+IMPORT FOREIGN SCHEMA public
+       LIMIT TO (intersections)
+       FROM SERVER map_srv INTO public;
