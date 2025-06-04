a67e1f72f1678d0fe33c31220e8c11c56fa20bc0
magis-techno <magis.techno@gmail.com>
2025-06-04 16:36:38 +0800

    support overlay

--- src/spdatalab/fusion/__init__.py
+++ src/spdatalab/fusion/__init__.py
@@ -2,15 +2,9 @@
 Spatial fusion and overlay analysis module for trajectory and spatial data.
 """
 
-from .trajectory_intersection import TrajectoryIntersectionAnalyzer
-from .overlay_analysis import OverlayAnalyzer
-from .intersection_processor import IntersectionProcessor
 from .spatial_join import SpatialJoin, SpatialRelation, JoinType, SummaryMethod
 
 __all__ = [
-    'TrajectoryIntersectionAnalyzer',
-    'OverlayAnalyzer', 
-    'IntersectionProcessor',
     'SpatialJoin',
     'SpatialRelation',
     'JoinType', 
